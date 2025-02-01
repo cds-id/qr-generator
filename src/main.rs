961ff7cda427a6379eed0fa843ed54cda39fa519
Indra Gunanda <indra.gunanda@gmail.com>
2025-02-01 22:29:02 +0700

    fix: to 0.0.0.0

--- src/main.rs
+++ src/main.rs
@@ -200,7 +200,7 @@ async fn main() -> std::io::Result<()> {
             .route("/generate-qr", web::get().to(generate_qr))
             .route("/health", web::get().to(health_check))
     })
-    .bind(("127.0.0.1", 8080))?
+    .bind(("0.0.0.0", 8080))?
     .run()
     .await
 }
